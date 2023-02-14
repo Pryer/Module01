05eabe590a26c3141b1ffde73032c50f642de140
Pryer <1558256601@qq.com>
2023-02-15 03:28:03 +0800

    Initial commit

---
+++ src/com/atguigu/java/TemplateTest.java
@@ -0,0 +1,27 @@
+package com.atguigu.java;
+
+import java.util.Arrays;
+
+/**
+ * ClassName: TemplateTest
+ * Package: com.fatigue.java
+ * Description:
+ *
+ * @Author jzp
+ * @Create 2023/2/14 4:42
+ * @Version 1.0
+ */
+
+public class TemplateTest {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("args = " + Arrays.toString(args));
+        System.out.println("TemplateTest.main");
+
+        int i = 10;
+        int num = 2;
+        System.out.println("i = " + i);
+    }
+
+
+}
---
+++ src/com/atguigu/java/hello.java
@@ -0,0 +1,13 @@
+package com.atguigu.java;
+
+/**
+ * ClassName: hello
+ * Package: com.atguigu.java
+ * Description:
+ *
+ * @Author jzp
+ * @Create 2023/2/14 2:53
+ * @Version 1.0
+ */
+public class hello {
+}
